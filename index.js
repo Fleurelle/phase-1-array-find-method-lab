c552baee3e07af6539c93efe957ff9ae99eb4214
Rose <r.d.jeanlouis01@gmail.com>
2021-04-21 17:40:16 -0400

    commit tree d1e2c15d46d28d3944856d759eada56148fc9f48

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+ function superbowlWin(record){ 
+    const newArray = record.find(function (element){
+            return element["result"] === "W"});
+     if (!!newArray){
+        return newArray["year"];
+     } else {
+        return undefined;
+       }
+}
 
